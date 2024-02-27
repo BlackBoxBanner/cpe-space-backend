eb2884ae684f49d958dde198ad27d9a60376f39a
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-02-27 23:18:02 +0700

    feat: add example body parser

--- src/controllers/main.controller.ts
+++ src/controllers/main.controller.ts
@@ -1,7 +1,15 @@
 import { APIController } from "@/types/responseType"
+import { UserSchema, UserType } from "@/types/zodSchema"
 
-export const mainGetController: APIController = async (req, res, _next) => {
-  return res.status(200).json({ data: { message: "Hello, World!" } })
+export const mainGetController: APIController<UserType> = async (req, res, _next) => {
+  const body = req.body
+  const parseBody = UserSchema.safeParse(body)
+
+  if (!parseBody.success) {
+    return res.status(400).json({ error: { zodError: parseBody.error.format() } })
+  }
+
+  return res.status(200).json({ data: parseBody.data })
 }
 
 export const mainPostController: APIController = async (req, res, _next) => {
