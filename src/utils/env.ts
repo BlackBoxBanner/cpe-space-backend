8baad3d9f3ca375b21cd025164eb21bc7f67f5da
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-03-19 15:01:43 +0700

    feat: using import @dookdiks/util

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,24 +1,10 @@
-export type CheckEnvVariableType = (envVar: string, defaultValue?: string | null) => string
+import { envVariable } from "@dookdiks/utils";
 
-export const checkEnvVariable: CheckEnvVariableType = (envVar, defaultValue = null) => {
-  const value = process.env[envVar];
-
-  if (!value || value === "") {
-    if (defaultValue !== null) {
-      return defaultValue;
-    } else {
-      throw new Error(`${envVar} does not exist in .env file.`);
-    }
-  }
-
-  return value;
-};
-
-export const PORT_ENV = checkEnvVariable("PORT", "5100");
-export const DATABASE_URL_ENV = checkEnvVariable("DATABASE_URL");
-export const MINIO_ENDPOINT_ENV = checkEnvVariable("MINIO_ENDPOINT");
-export const MINIO_PORT_ENV = checkEnvVariable("MINIO_PORT");
-export const MINIO_SECRET_KEY_ENV = checkEnvVariable("MINIO_SECRET_KEY");
-export const MINIO_ACCESS_KEY_ENV = checkEnvVariable("MINIO_ACCESS_KEY");
-export const API_TOKEN_ENV = checkEnvVariable("API_TOKEN");
-export const JWT_SECRET_ENV = checkEnvVariable("JWT_SECRET")
+export const PORT_ENV = envVariable("PORT", "5100");
+export const DATABASE_URL_ENV = envVariable("DATABASE_URL");
+export const MINIO_ENDPOINT_ENV = envVariable("MINIO_ENDPOINT");
+export const MINIO_PORT_ENV = envVariable("MINIO_PORT");
+export const MINIO_SECRET_KEY_ENV = envVariable("MINIO_SECRET_KEY");
+export const MINIO_ACCESS_KEY_ENV = envVariable("MINIO_ACCESS_KEY");
+export const API_TOKEN_ENV = envVariable("API_TOKEN");
+export const JWT_SECRET_ENV = envVariable("JWT_SECRET")
