aff5867a62f8403d44e3d0cf0b8ce425c74a8980
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-04-04 16:10:30 +0700

    feat: fix routeing

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,11 @@ import cookieParser from "cookie-parser"
 import mainRouter from '@/routes/main.route'
 import userRouter from '@/routes/user.route'
 import authRouter from '@/routes/auth.route'
+import configRouter from '@/routes/config.route'
 import { middleware } from '@/middleware/middleware';
+import { publicKeyPem, privateKeyPem } from '@/utils/decryption/generate';
+import cors from "cors"
+
 
 // Initialize Express 
 export const app = express()
@@ -15,6 +19,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.raw())
 app.use(cookieParser())
+app.use(cors())
+
+// Set app variables
+app.set("publicKeyPem", publicKeyPem)
+app.set("privateKeyPem", privateKeyPem)
 
 // Config Middleware for handling CORS
 app.use(middleware)
@@ -23,5 +32,6 @@ app.use(middleware)
 app.use('/', mainRouter)
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/config', configRouter)
 
 export default app;
