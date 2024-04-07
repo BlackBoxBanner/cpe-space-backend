1d34391757b238cffe3b24172d53c25adbf00ca5
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-04-07 19:10:21 +0700

    update auth.controller.ts and auth.route.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { APIController } from "@/types/responseType"
 import { UserSchema, UserType } from "@/types/zodSchema"
+import { decrypt } from "@/utils/decryption"
 import { JWT_SECRET_ENV } from "@/utils/env"
 import prisma from "@/utils/prisma"
 import bcrypt from "bcrypt"
@@ -52,6 +53,38 @@ export const loginController: APIController<Pick<UserType, "password" | "student
             return res.status(400).json({ error: { customError: error.message } })
         }
         return res.status(200).json({ error: { customError: "Internal Error" } })
+    }
+
+}
+
+export const changePasswordController: APIController<string> = async (req, res, _next) => {
+    try {
+        const body = decrypt(req.body.data)
+
+        const validateChangePassword = UserSchema.pick({ studentid: true, password: true }).safeParse(body)
+
+        if (!validateChangePassword.success) return res.status(400).json({ error: { zodError: validateChangePassword.error.format() } })
+
+        const { studentid, password } = validateChangePassword.data
+
+        const hash = await bcrypt.hash(password, 10)
+
+        await prisma.user.update({
+            where: {
+                studentid
+            },
+            data: {
+                password: hash
+            }
+        })
+
+        return res.status(200).json({ data: "Successfully change user password." })
+
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(400).json({ error: { customError: error.message } })
+        }
+        return res.status(400).json({ error: { customError: "Internal Error" } })
     }
 
 }
