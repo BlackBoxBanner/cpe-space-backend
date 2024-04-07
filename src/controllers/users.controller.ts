d20ab225eb7c83a9a62ca6f505379e06bfb01398
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-04-07 23:12:43 +0700

    feat: implement a get user controller

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,33 @@
+import { APIController, PostBody } from "@/types/responseType";
+import prisma from "@/utils/prisma";
+import { User } from "@prisma/client";
+
+type UserExcludePassword = Omit<User, "password">;
+export const usersGetController: APIController<
+    UserExcludePassword | null
+> = async (req, res, _next) => {
+
+    const id = req.params.id;
+
+    const userData = await prisma.user.findUnique({
+        where: { id },
+        select: {
+            id: true,
+            studentid: true,
+            name: true,
+            email: true,
+            phone: true,
+            program: true,
+            image: true,
+            touched: true,
+            role: true,
+            student: true,
+        },
+    });
+
+    if (!userData) {
+        return res.status(404).json({ error: { customError: "User not found" } });
+    }
+
+    return res.status(200).json({ data: userData });
+};
