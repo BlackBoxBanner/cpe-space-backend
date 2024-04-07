30824ab9f689e70d3a1db86d98de287991abcd35
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-04-07 21:06:23 +0700

    update auth.controller.ts and auth.route.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,15 +1,18 @@
 import { APIController } from "@/types/responseType"
 import { UserSchema, UserType } from "@/types/zodSchema"
+import { decrypt } from "@/utils/decryption"
 import { JWT_SECRET_ENV } from "@/utils/env"
 import prisma from "@/utils/prisma"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 
-export const loginController: APIController<Pick<UserType, "password" | "studentid"> | Omit<UserType, "password">> = async (req, res, _next) => {
+export const loginController: APIController<string> = async (req, res, _next) => {
     try {
-        const validateLogin = UserSchema.pick({ studentid: true, password: true }).safeParse(req.body)
+        const body = decrypt(req.body.data)
 
-        if (!validateLogin.success) return res.status(400).json({ error: { zodError: validateLogin.error.format() } })
+        const validateLogin = UserSchema.pick({ studentid: true, password: true }).safeParse(body)
+
+        if (!validateLogin.success) return res.status(403).json({ error: { zodError: validateLogin.error.format() } })
 
         const { password, studentid } = validateLogin.data
 
@@ -44,12 +47,13 @@ export const loginController: APIController<Pick<UserType, "password" | "student
             httpOnly: true,
             sameSite: "none",
         });
+
         const { password: newpassword, ...returnData } = user as UserType
-        return res.status(200).json({ data: returnData })
+        return res.status(200).json({ data: token })
 
     } catch (error) {
         if (error instanceof Error) {
-            return res.status(400).json({ error: { customError: error.message } })
+            return res.status(403).json({ error: { customError: error.message } })
         }
         return res.status(200).json({ error: { customError: "Internal Error" } })
     }
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { loginController } from "@/controllers/auth.controller";
 const routers = Router();
 
-routers.post("/sign-in", loginController);
+routers.post("/signin", loginController);
 
 export default routers;
