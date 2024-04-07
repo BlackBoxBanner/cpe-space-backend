1e86d4bfa103a7c920ed6bfe5063b5c1395d6a99
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-04-08 00:13:51 +0700

    update auth.controller.ts and auth.route.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -58,3 +58,18 @@ export const loginController: APIController<string> = async (req, res, _next) =>
     }
 
 }
+
+export const signoutController: APIController<string> = async (req, res, _next) => {
+    try {
+        res.clearCookie("cpe_space_session")
+        res.clearCookie("user-id")
+
+        return res.status(200).json({ data: "signout success" })
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(403).json({ error: { customError: error.message } })
+        }
+        return res.status(200).json({ error: { customError: "Internal Error" } })
+    }
+
+}
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { loginController } from "@/controllers/auth.controller";
+import { loginController, signoutController } from "@/controllers/auth.controller";
 const routers = Router();
 
 routers.post("/signin", loginController);
+routers.post("/signout", signoutController);
 
 export default routers;
