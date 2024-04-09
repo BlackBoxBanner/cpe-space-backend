875dba83340b50f12848bab05794d0ac9681b9b2
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-04-09 19:18:12 +0700

    update image.controller.ts and image.route.ts

--- src/routes/image.route.ts
+++ src/routes/image.route.ts
@@ -1,10 +1,7 @@
-import { getController, getImagePathController, getPathController, uploadController } from "@/controllers/image.controller";
+import { uploadController } from "@/controllers/image.controller";
 import { Router } from "express";
 const routers = Router();
 
 routers.post("/upload", uploadController);
-routers.get("/get", getPathController);
-routers.get("/get/:path", getImagePathController);
-routers.get("/get/:path/:name", getController);
 
 export default routers;
