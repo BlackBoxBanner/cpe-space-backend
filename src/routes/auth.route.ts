c777392cee93c5de6d7eac8cbff81244b76f5a19
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-04-09 20:39:35 +0700

    update auth.controller.ts and auth.route.ts

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,7 @@
+import { loginController } from "@/controllers/auth.controller";
 import { Router } from "express";
-import { loginController, registerController, signoutController,changePasswordController } from "@/controllers/auth.controller";
 const routers = Router();
 
 routers.post("/signin", loginController);
-routers.post("/signout", signoutController);
-routers.post("/register", registerController)
-routers.post("/change-password", changePasswordController);
 
 export default routers;
