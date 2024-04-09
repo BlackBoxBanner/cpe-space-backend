bacccad0f9286ad30c88ff6c38c816e869e7e6ef
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-04-09 16:03:42 +0700

    update image.controller.ts and image.route.ts

--- src/controllers/image.controller.ts
+++ src/controllers/image.controller.ts
@@ -1,16 +1,13 @@
 import { APIController, APIControllerImage } from "@/types/responseType";
 import { useMinio } from "@/utils/minio";
+import { BucketItem } from "minio";
 
 type UploadBody = {
   name: string;
   base64: string;
 };
 
-export const uploadController: APIController<string, UploadBody> = async (
-  req,
-  res,
-  _next,
-) => {
+export const uploadController: APIController<string, UploadBody> = async (req, res, _next) => {
   try {
     const { base64, name } = req.body;
 
@@ -51,3 +48,40 @@ export const getController: APIControllerImage = async (req, res, _next) => {
   }
 };
 
+export const getPathController: APIController<string[]> = async (req, res, _next) => {
+  try {
+    const minio = useMinio();
+    const list = await minio.listBuckets();
+    return res.status(200).json({ data: list.reduce((acc, cur) => acc.concat(cur.name), [] as string[]) });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(403).json({ error: { customError: error.message } });
+    }
+    return res.status(200).json({ error: { customError: "Internal Error" } });
+  }
+}
+
+export const getImagePathController: APIController<string[]> = async (req, res, _next) => {
+  try {
+    const { path } = req.params as { path: string };
+    const minio = useMinio();
+    const fileStream = minio.listObjects(path)
+
+    if (!(await minio.bucketExists(path))) throw new Error("Bucket not found")
+
+    let listFile: string[] = []
+    fileStream.on("data", (obj) => {
+      obj.name && listFile.push(obj.name)
+    })
+    fileStream.on("end", () => {
+      res.json({ data: listFile });
+    })
+
+    return res.status(200)
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(403).json({ error: { customError: error.message } });
+    }
+    return res.status(200).json({ error: { customError: "Internal Error" } });
+  }
+}
--- src/routes/image.route.ts
+++ src/routes/image.route.ts
@@ -1,8 +1,10 @@
-import { getController, uploadController } from "@/controllers/image.controller";
+import { getController, getImagePathController, getPathController, uploadController } from "@/controllers/image.controller";
 import { Router } from "express";
 const routers = Router();
 
 routers.post("/upload", uploadController);
+routers.get("/get", getPathController);
+routers.get("/get/:path", getImagePathController);
 routers.get("/get/:path/:name", getController);
 
 export default routers;
