3aac86983cbf519045ad237dff768b094d2e9873
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-04-10 21:40:22 +0700

    create base event api

---
+++ src/routes/event.route.ts
@@ -0,0 +1,44 @@
+import { Router } from "express";
+
+const routers = Router();
+
+// get all events
+routers.get("/", () => { });
+
+// get event
+routers.get("/:id", () => { });
+
+// create event
+routers.post("/", () => { });
+
+// update event
+routers.patch("/:id", () => { });
+
+// delete event
+routers.delete("/:id", () => { });
+
+// get all posts
+routers.get("/post", () => { });
+
+// get post
+routers.get("/post/:id", () => { });
+
+// create post
+routers.post("/post", () => { });
+
+// update post
+routers.patch("/post/:id", () => { });
+
+// delete post
+routers.delete("/post/:id", () => { });
+
+// get all attendees
+routers.get("/attendees", () => { });
+
+// join event
+routers.post("/join/:id", () => { });
+
+// leave event
+routers.post("/leave/:id", () => { });
+
+export default routers;
