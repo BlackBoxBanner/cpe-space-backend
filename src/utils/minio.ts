f6e5a3bf88a152abe7f125e8e8127e6cfc653882
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-04-16 01:07:04 +0800

    update 5 files (#67)

--- src/utils/minio.ts
+++ src/utils/minio.ts
@@ -1,13 +1,13 @@
 import { Client } from "minio";
-import { MINIO_ACCESS_KEY_ENV, MINIO_ENDPOINT_ENV, MINIO_PORT_ENV, MINIO_SECRET_KEY_ENV, } from "@/utils/env";
+import { env } from "@/utils/env";
 
 const useMinio = () => {
   return new Client({
-    endPoint: MINIO_ENDPOINT_ENV,
-    port: Number.parseInt(MINIO_PORT_ENV),
+    endPoint: env.MINIO_ENDPOINT,
+    port: Number.parseInt(env.MINIO_PORT),
     useSSL: false,
-    accessKey: MINIO_ACCESS_KEY_ENV,
-    secretKey: MINIO_SECRET_KEY_ENV,
+    accessKey: env.MINIO_ACCESS_KEY,
+    secretKey: env.MINIO_SECRET_KEY,
   })
 }
 export { useMinio }
