29c09562957f5deea54c6ec2bbc474abdb0ea38c
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-04-22 19:57:27 +0700

    feat: implement usersPostController

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,9 @@
 import { APIController, PostBody } from "@/types/responseType";
+import { UserFormSchema } from "@/types/zodSchema";
+import { customError } from "@/utils/customError";
 import prisma from "@/utils/prisma";
-import { User } from "@prisma/client";
+import { Prisma, User } from "@prisma/client";
+import { z } from "zod";
 
 type UserExcludePassword = Omit<User, "password">;
 export const usersGetController: APIController<UserExcludePassword> = async (
@@ -33,4 +36,31 @@ export const usersGetController: APIController<UserExcludePassword> = async (
     }
 
     return res.status(200).json({ data: userData });
+};
+
+export const usersPostController: APIController<Prisma.BatchPayload> = async (
+    req,
+    res,
+    _next
+) => {
+    try {
+        const UserFormSchemaArray = z.array(UserFormSchema);
+
+        const validateUser = UserFormSchemaArray.safeParse(req.body);
+
+        if (!validateUser.success)
+            return res
+                .status(400)
+                .json({ error: { zodError: validateUser.error.format() } });
+
+        const userData = await prisma.user.createMany({
+            data: validateUser.data,
+            skipDuplicates: true,
+        });
+
+        return res.status(201).json({ data: userData });
+
+    } catch (error) {
+        return res.status(400).json(customError(error));
+    }
 };
