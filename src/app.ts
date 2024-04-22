d942bc652378ace06cb232cd96a7d1d31a0ab7f7
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-04-22 19:58:36 +0700

    refactor: app.ts to remove duplicate user route middleware

--- src/app.ts
+++ src/app.ts
@@ -43,7 +43,6 @@ app.set("privateKeyPem", privateKeyPem)
 // Mouth route handlers
 app.use('/', mainRouter)
 app.use('/api/user', middleware, authMiddleware, usersRouter)
-app.use('/api/user', middleware, authMiddleware, usersRouter)
 app.use('/api/auth', middleware, authRouter)
 app.use('/api/config', middleware, configRouter)
 app.use('/api/announcement', middleware, authMiddleware, announcementRouter)
