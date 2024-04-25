0fe758b32ed57cf995a10c5a7666da1fe8f7d547
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-04-25 13:42:22 +0700

    68 create post user api (#70)
    * feat: implement usersPostController
    
    * feat: add post method for user route
    
    * refactor: app.ts to remove duplicate user route middleware
    
    * fix merge conflict
    
    ---------
    
    Co-authored-by: Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
    
    This close #68

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,7 +1,9 @@
 import { APIController, PostBody } from "@/types/responseType";
+import { UserFormSchema } from "@/types/zodSchema";
 import { customError } from "@/utils/customError";
 import prisma from "@/utils/prisma";
-import { User } from "@prisma/client";
+import { Prisma, User } from "@prisma/client";
+import { z } from "zod";
 
 type UserExcludePassword = Omit<User, "password">;
 export const usersGetController: APIController<UserExcludePassword | UserExcludePassword[]> = async (
@@ -84,3 +86,30 @@ export const usersGetController: APIController<UserExcludePassword | UserExclude
 
     };
 }
+
+export const usersPostController: APIController<Prisma.BatchPayload> = async (
+    req,
+    res,
+    _next
+) => {
+    try {
+        const UserFormSchemaArray = z.array(UserFormSchema);
+
+        const validateUser = UserFormSchemaArray.safeParse(req.body);
+
+        if (!validateUser.success)
+            return res
+                .status(400)
+                .json({ error: { zodError: validateUser.error.format() } });
+
+        const userData = await prisma.user.createMany({
+            data: validateUser.data,
+            skipDuplicates: true,
+        });
+
+        return res.status(201).json({ data: userData });
+
+    } catch (error) {
+        return res.status(400).json(customError(error));
+    }
+};
