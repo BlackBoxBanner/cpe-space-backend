b650b9a260ba716adeef37b2352423af4ba1380e
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-04-30 23:12:55 +0700

    Update package.json and pnpm-lock.yaml to add @types/validator package

--- src/types/zodSchema.ts
+++ src/types/zodSchema.ts
@@ -1,130 +1,86 @@
-import zod from 'zod';
+import zod from "zod";
+import validator from 'validator';
 
-const invalid_type_error = (field: string, type: string) => {
-  return `${field} must be a ${type}`
-}
+const RoleEnum = zod.enum(['STUDENT', 'TEACHER', 'ADMIN', 'OFFICER']);
 
-const invalid_type_error_enum = (field: string, enumValues: string[]) => {
-  return `${field} must be a enum and one of ${enumValues.join(", ")}`
-}
-
-const required_error = "This field is required"
-
-const invalid_value_length_error = (field: string, length: number) => {
-  return `${field} have to be at least ${length} character long`
-}
+const CommunitiesStatusEnum = zod.enum(['PUBLIC', 'PRIVATE']);
 
 export const UserSchema = zod.object({
-  id: zod.string({ invalid_type_error: invalid_type_error("ID", "string"), required_error }).min(1, invalid_value_length_error("ID", 1)),
-  studentid: zod.string({ invalid_type_error: invalid_type_error("Student ID", "string"), required_error }).min(1, invalid_value_length_error("Student ID", 1)),
-  name: zod.string({ invalid_type_error: invalid_type_error("Name", "string"), required_error }),
-  email: zod.string({ invalid_type_error: invalid_type_error("Email", "string") }).email({ message: "Invalid email" }).optional(),
-  phone: zod.string().optional(),
-  program: zod.enum(['REGULAR', 'INTERNATIONAL', 'HEALTH_DATA_SCIENCE', 'RESFENTIAL_COLLEGE'], { invalid_type_error: invalid_type_error_enum("Program", ['REGULAR', 'INTERNATIONAL', 'HEALTH_DATA_SCIENCE', 'RESFENTIAL_COLLEGE']), required_error }).default('REGULAR'),
-  password: zod.string({ invalid_type_error: invalid_type_error("Password", "string"), required_error }).min(1, invalid_value_length_error("Password", 1)),
+  id: zod.string().uuid().refine(validator.isUUID),
+  studentid: zod.string(),
+  name: zod.string(),
+  email: zod.string().email().refine(validator.isEmail),
+  phone: zod.string().optional().refine(value => value && validator.isMobilePhone(value)),
+  program: RoleEnum.default('STUDENT'),
+  password: zod.string(),
   image: zod.string().optional(),
-  touched: zod.boolean({ invalid_type_error: invalid_type_error("Touched", "boolean"), required_error }),
-  role: zod.enum(['ADMIN', 'STUDENT'], { invalid_type_error: invalid_type_error_enum("role", ['ADMIN', 'STUDENT']), required_error }).default('STUDENT'),
-  student: zod.enum(['GRADUATED', 'ENROLLED'], { invalid_type_error: invalid_type_error_enum("student", ['GRADUATED', 'ENROLLED']), required_error }).default('ENROLLED'),
-})
+  touched: zod.boolean().default(false),
+  role: RoleEnum.default('STUDENT'),
+});
 
-export const UserFormSchema = UserSchema.omit({ id: true, touched: true, role: true, student: true, })
+export const CommunitiesSchema = zod.object({
+  id: zod.string().uuid().refine(validator.isUUID),
+  userId: zod.string().uuid().refine(validator.isUUID),
+  name: zod.string(),
+  image: zod.string(),
+  status: CommunitiesStatusEnum.default('PUBLIC'),
+  createdAt: zod.date().default(() => new Date()),
+});
 
-export type UserType = zod.infer<typeof UserSchema>
+export const TopicSchema = zod.object({
+  id: zod.string().uuid().refine(validator.isUUID),
+  name: zod.string(),
+});
 
-export type UserFormType = zod.infer<typeof UserFormSchema>
-
-export const BlogSchema = zod.object({
-  id: zod.string({ invalid_type_error: invalid_type_error("ID", "string"), required_error }).min(1, invalid_value_length_error("ID", 1)),
-  authorId: zod.string(),
-  title: zod.string(),
+export const PostSchema = zod.object({
+  id: zod.string().uuid().refine(validator.isUUID),
   content: zod.string(),
-  published: zod.boolean(),
-  createdAt: zod.string(),
-  updatedAt: zod.string(),
-})
+  userId: zod.string().uuid().refine(validator.isUUID),
+  communitiesId: zod.string().refine(validator.isUUID).optional(),
+  likes: zod.number().default(0),
+  createdAt: zod.date().default(() => new Date()),
+  updatedAt: zod.date().optional(),
+});
 
-export const BlogFormSchema = BlogSchema.omit({ id: true, createdAt: true, updatedAt: true })
+export const PostTopicSchema = zod.object({
+  postId: zod.string().uuid().refine(validator.isUUID),
+  topicId: zod.string().uuid().refine(validator.isUUID),
+});
 
-export type BlogType = zod.infer<typeof BlogSchema>
+export const PostLikesSchema = zod.object({
+  id: zod.string().uuid().refine(validator.isUUID),
+  postId: zod.string().uuid().refine(validator.isUUID),
+  userId: zod.string().uuid().refine(validator.isUUID),
+});
 
-export type BlogFormType = zod.infer<typeof BlogFormSchema>
+export const CommentSchema = zod.object({
+  id: zod.string().uuid().refine(validator.isUUID),
+  postId: zod.string().uuid().refine(validator.isUUID),
+  userId: zod.string().uuid().refine(validator.isUUID),
+  content: zod.string(),
+  likes: zod.number().default(0),
+  createdAt: zod.date().default(() => new Date()),
+  updatedAt: zod.date().optional(),
+});
 
-export type ConversationsType = {
-  id: string
-  createdAt: string
-  updatedAt: string
-}
+export const CommentLikesSchema = zod.object({
+  id: zod.string().uuid().refine(validator.isUUID),
+  commentId: zod.string().uuid().refine(validator.isUUID),
+  userId: zod.string().uuid().refine(validator.isUUID),
+});
 
-export type ConversationsParticipantsSchema = {
-  id: string
-  conversationId: string
-  userId: string
-  createdAt: string
-}
+export const ReplaySchema = zod.object({
+  id: zod.string().uuid().refine(validator.isUUID),
+  commentId: zod.string().uuid().refine(validator.isUUID),
+  userId: zod.string().uuid().refine(validator.isUUID),
+  content: zod.string(),
+  likes: zod.number().default(0),
+  createdAt: zod.date().default(() => new Date()),
+  updatedAt: zod.date().optional(),
+});
 
-export const MessageSchema = zod.object({
-  id: zod.string({ invalid_type_error: invalid_type_error("ID", "string"), required_error }).min(1, invalid_value_length_error("ID", 1)),
-  conversationId: zod.string({ invalid_type_error: invalid_type_error("Conversation ID", "string"), required_error }).min(1, invalid_value_length_error("Conversation ID", 1)),
-  authorId: zod.string({ invalid_type_error: invalid_type_error("Author ID", "string"), required_error }).min(1, invalid_value_length_error("Author ID", 1)),
-  content: zod.string({ invalid_type_error: invalid_type_error("Content", "string"), required_error }).min(1, invalid_value_length_error("Content", 1)),
-  read: zod.boolean({ invalid_type_error: invalid_type_error("Read", "boolean"), required_error }),
-  createdAt: zod.string({ invalid_type_error: invalid_type_error("Created At", "string"), required_error }).min(1, invalid_value_length_error("Created At", 1)),
-  updatedAt: zod.string({ invalid_type_error: invalid_type_error("Updated At", "string"), required_error }).min(1, invalid_value_length_error("Updated At", 1)),
-})
-
-export const MessageFormSchema = MessageSchema.omit({ id: true, createdAt: true, updatedAt: true })
-
-export type MessageType = zod.infer<typeof MessageSchema>
-
-export type MessageFormType = zod.infer<typeof MessageFormSchema>
-
-export const AnouncementSchema = zod.object({
-  id: zod.string({ invalid_type_error: invalid_type_error("ID", "string"), required_error }).min(1, invalid_value_length_error("ID", 1)),
-  authorId: zod.string({ invalid_type_error: invalid_type_error("Author ID", "string"), required_error }).min(1, invalid_value_length_error("Author ID", 1)),
-  title: zod.string({ invalid_type_error: invalid_type_error("Title", "string"), required_error }).min(1, invalid_value_length_error("Title", 1)),
-  content: zod.string({ invalid_type_error: invalid_type_error("Content", "string"), required_error }).min(1, invalid_value_length_error("Content", 1)),
-  published: zod.boolean({ invalid_type_error: invalid_type_error("Published", "boolean"), required_error }),
-  createdAt: zod.string({ invalid_type_error: invalid_type_error("Created At", "string"), required_error }).min(1, invalid_value_length_error("Created At", 1)),
-  updatedAt: zod.string({ invalid_type_error: invalid_type_error("Updated At", "string"), required_error }).min(1, invalid_value_length_error("Updated At", 1)),
-})
-
-export const AnouncementFormSchema = AnouncementSchema.omit({ id: true, authorId: true, createdAt: true, updatedAt: true })
-
-export type AnouncementType = zod.infer<typeof AnouncementSchema>
-
-export type AnouncementFormType = zod.infer<typeof AnouncementFormSchema>
-
-export const EventPostSchema = zod.object({
-  id: zod.string({ invalid_type_error: invalid_type_error("ID", "string"), required_error }).min(1, invalid_value_length_error("ID", 1)),
-  authorId: zod.string({ invalid_type_error: invalid_type_error("Author ID", "string"), required_error }).min(1, invalid_value_length_error("Author ID", 1)),
-  title: zod.string({ invalid_type_error: invalid_type_error("Title", "string"), required_error }).min(1, invalid_value_length_error("Title", 1)),
-  content: zod.string({ invalid_type_error: invalid_type_error("Content", "string"), required_error }).min(1, invalid_value_length_error("Content", 1)),
-  published: zod.boolean({ invalid_type_error: invalid_type_error("Published", "boolean"), required_error }),
-  eventId: zod.string({ invalid_type_error: invalid_type_error("Event ID", "string"), required_error }).min(1, invalid_value_length_error("Event ID", 1)),
-  createdAt: zod.date({ invalid_type_error: invalid_type_error("Created At", "string"), required_error }),
-  updatedAt: zod.date({ invalid_type_error: invalid_type_error("Updated At", "string"), required_error }),
-})
-
-export type EventPostType = zod.infer<typeof EventPostSchema>
-
-export const EventSchema = zod.object({
-  id: zod.string({ invalid_type_error: invalid_type_error("ID", "string"), required_error }).min(1, invalid_value_length_error("ID", 1)),
-  authorId: zod.string({ invalid_type_error: invalid_type_error("Author ID", "string"), required_error }).min(1, invalid_value_length_error("Author ID", 1)),
-  title: zod.string({ invalid_type_error: invalid_type_error("Title", "string"), required_error }).min(1, invalid_value_length_error("Title", 1)),
-  content: zod.string({ invalid_type_error: invalid_type_error("Content", "string"), required_error }).min(1, invalid_value_length_error("Content", 1)),
-  eventDate: zod.date({ invalid_type_error: invalid_type_error("Event Date", "date"), required_error }),
-  createdAt: zod.date({ invalid_type_error: invalid_type_error("Created At", "date"), required_error }),
-  updatedAt: zod.date({ invalid_type_error: invalid_type_error("Updated At", "date"), required_error }),
-})
-
-export type EventType = zod.infer<typeof EventSchema>
-
-export const EventParticipantsSchema = zod.object({
-  id: zod.string({ invalid_type_error: invalid_type_error("ID", "string"), required_error }).min(1, invalid_value_length_error("ID", 1)),
-  eventId: zod.string({ invalid_type_error: invalid_type_error("Event ID", "string"), required_error }).min(1, invalid_value_length_error("Event ID", 1)),
-  userId: zod.string({ invalid_type_error: invalid_type_error("User ID", "string"), required_error }).min(1, invalid_value_length_error("User ID", 1)),
-  createdAt: zod.date({ invalid_type_error: invalid_type_error("Created At", "string"), required_error }),
-})
-
-export type EventParticipantsType = zod.infer<typeof EventParticipantsSchema>
+export const ReplayLikesSchema = zod.object({
+  id: zod.string().uuid().refine(validator.isUUID),
+  replayId: zod.string().uuid().refine(validator.isUUID),
+  userId: zod.string().uuid().refine(validator.isUUID),
+});
