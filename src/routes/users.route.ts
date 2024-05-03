2b430d3364ca8c61860519e90812954994a8e692
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-05-04 00:42:53 +0700

    Add searchUserGetController to users.controller.ts and users.route.ts

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,9 +1,10 @@
-import { usersGetController } from '@/controllers/users.controller'
+import { usersGetController, searchUserGetController } from '@/controllers/users.controller'
 import { Router } from 'express'
 
 const routers = Router()
 
 routers.get('/', usersGetController)
+routers.get('/search', searchUserGetController)
 // routers.post('/', usersPostController)
 
 export default routers
