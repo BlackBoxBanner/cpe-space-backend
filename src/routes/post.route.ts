e9de2142b6e65f399ffd7fb533642ef2fd778324
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-05-06 17:25:59 +0700

    refactor: Remove unused code and optimize post route handlers

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -11,14 +11,17 @@ import { Router } from 'express';
 
 const routers = Router();
 
+// Post Routes
 routers.get('/', getPostController);
 routers.post('/', createPostController);
-// routers.patch('/:id', () => {});
 routers.delete('/:id', deletePostController);
+
+// Comment in Post Route
 routers.get('/:id/comment', getCommentController);
 routers.post('/:id/comment', createCommentController);
 routers.delete('/:id/comment', deleteCommentController);
 
+// Like of the  Post Route
 routers.post("/:id/like", createLikeController);
 routers.delete("/:id/like", removeLikeController);
 
